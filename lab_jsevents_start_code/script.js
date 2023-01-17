2117ca511f57cd206ac6e9187d5d62b0a2c49f40
Radhika <radhikarathi308@gmail.com>
2023-01-17 15:13:57 +0000

    adds task

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,15 +1,19 @@
-// const enterButton = document.querySelector("#enter"); 
+const enterButton = document.querySelector("#enter"); 
 
 const taskForm = document.querySelector("task-form");
 
 const addTask = (event) => {
     event.preventDefault();
+    console.log(event.target.newTask.value);
     const task = document.createElement("li");
-    const inputText = document.querySelector("#new-todo");
-    task.textContent = event.target.newTask.value;
+    const inputText = document.querySelectorAll("input[type=text]");
+    task.textContent = event.target;
+    console.log(inputText);
+
     const list = document.querySelector("#list");
     list.appendChild(task);
     console.log(inputText);
 }
 
-taskForm.addEventListener('submit', addTask);
+enterButton.addEventListener('button', addTask);
+
